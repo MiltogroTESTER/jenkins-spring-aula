7eccae1e74b939768724d9d9b2a3e46753f8eed0
MiltogroTESTER <167721233+MiltogroDEV@users.noreply.github.com>
2025-04-16 21:48:49 -0300

    Update Project

---
+++ src/main/java/com/example/exemplo_jenkins/ExemploJenkinsApplication.java
@@ -0,0 +1,14 @@
+package com.example.exemplo_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class ExemploJenkinsApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/exemplo_jenkins/controller/Controller.java
@@ -0,0 +1,17 @@
+package com.example.exemplo_jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+//@RequestMapping se tiver varios controllers
+public class Controller {
+
+    @GetMapping("/{name}")
+    public String helloUser(@PathVariable("name") String name) {
+        return "Seja bem vindo " + name;
+    }
+}
